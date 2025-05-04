d38bc307dfbd9a532d55e0829f16d8cf0b247998
Carla Lutz <clutzdias@gmail.com>
2025-05-04 01:21:31 -0300

    Task Form creation

--- Frontend/todolist/src/app/tasks/task/task.component.ts
+++ Frontend/todolist/src/app/tasks/task/task.component.ts
@@ -3,6 +3,7 @@ import { ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
 import { Task } from 'src/app/interfaces/task';
 import { TaskService } from 'src/app/services/taskservice';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-task',
@@ -10,12 +11,23 @@ import { TaskService } from 'src/app/services/taskservice';
   styleUrls: ['./task.component.css']
 })
 export class TaskComponent implements OnInit {
-
+  private createMode: boolean = true;
+  public formGroup: FormGroup;
   task: Task | undefined;
 
   constructor(private route: ActivatedRoute,
               private taskService: TaskService,
-              private location: Location) { }
+              private location: Location,
+              private formBuilder: FormBuilder) {
+    this.formGroup = this.formBuilder.group({
+      title: ['', Validators.compose([Validators.required,
+                                      Validators.minLength(3), 
+                                      Validators.maxLength(50)])],
+      description: ['', Validators.compose([Validators.minLength(3), 
+                                            Validators.maxLength(150)])],
+      completed: [false]
+    });
+  }
 
   getTask(id: string): void {
     this.taskService.getTask(+id)
@@ -26,13 +38,28 @@ export class TaskComponent implements OnInit {
     this.location.back();
   }
 
+  saveTask() {
+
+
+    if (this.createMode) {
+      this.taskService.createTask(this.formGroup.value)
+        .subscribe(() => this.goBack());
+    } else {
+      const formData = {id: this.task?.id, ...this.formGroup.value};  
+      this.taskService.updateTask(formData)
+        .subscribe(() => this.goBack());
+    }
+  }
+
   ngOnInit() {
     const id = this.route.snapshot.paramMap.get('id');
     if (id) {
       this.getTask(id);
+      this.createMode = false;
     } else {
       this.task = undefined;
     }
   }
 
 }
+
